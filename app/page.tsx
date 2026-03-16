d281bb25a6e0aec68be5268f74320043e6205849
dpkdonlarj <deepakdeepu200318@gmail.com>
2026-03-17 01:54:30 +0530

    Corrected folder structure and verified Sanity connection

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import { Syne } from 'next/font/google';
 import Navbar from '@/components/Navbar';
 import ShopGrid from '@/components/ShopGrid';
 // FIXED PATH: Pointing to the root sanity folder
-import { client } from '@/sanity/lib/client'; 
+// We added "/app" to the path because that's where the folder is now
+import { client } from '@/sanity/lib/client';
 
 const syne = Syne({ 
   subsets: ['latin'],
