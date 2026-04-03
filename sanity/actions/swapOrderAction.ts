792cb8df62ab8e21b48ebc94761fa60516d5b902
dpkdonlarj <deepakdeepu200318@gmail.com>
2026-04-03 11:29:25 +0530

    fix: resolve typescript type mismatch in swap action

--- sanity/actions/swapOrderAction.ts
+++ sanity/actions/swapOrderAction.ts
@@ -4,8 +4,6 @@ import { useDocumentOperation, useClient } from 'sanity'
 export function SwapOrderAction(props: any) {
   const { publish } = useDocumentOperation(props.id, props.type)
   const [isPublishing, setIsPublishing] = useState(false)
-  
-  // FIXED: This is the correct way to get the client in Sanity v3
   const client = useClient({ apiVersion: '2024-01-01' })
 
   useEffect(() => {
@@ -15,7 +13,9 @@ export function SwapOrderAction(props: any) {
   }, [isPublishing, props.draft])
 
   return {
-    disabled: publish.disabled,
+    // FIXED: The !! converts the string reason into a boolean (true/false)
+    // This stops the TypeScript error on Vercel
+    disabled: !!publish?.disabled,
     label: isPublishing ? 'Swapping...' : 'Publish & Swap',
     onHandle: async () => {
       setIsPublishing(true)
@@ -23,22 +23,20 @@ export function SwapOrderAction(props: any) {
       const newNum = props.draft?.orderNumber
       const oldNum = props.published?.orderNumber
 
-      // Logic: If the number changed, find the "victim" at the new spot
       if (newNum && newNum !== oldNum) {
-        // We look for the shop that currently has the number we want
         const victim = await client.fetch(
           `*[_type == "store" && orderNumber == $newNum && _id != $id && !(_id in path("drafts.**"))][0]`,
           { newNum, id: props.id }
         )
 
-        // If that spot is taken, tell the "victim" to take our old number
         if (victim && oldNum) {
           await client.patch(victim._id).set({ orderNumber: oldNum }).commit()
         }
       }
 
-      // Finally, publish our current shop with the new number
-      publish.execute()
+      if (publish) {
+        publish.execute()
+      }
       props.onComplete()
     },
   }
